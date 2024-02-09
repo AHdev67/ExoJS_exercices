cbc35dc0597702a8978ae12e09890f58b0cbf283
AHdev67 <antoinehelfer.contact@gmail.com>
2024-02-09 14:00:40 +0100

    added array for each square & forEach method for applying class toggle

--- exo2/main.js
+++ exo2/main.js
@@ -1,5 +1,12 @@
-const carre = document.querySelector(".square");
+const carre1 = document.querySelector("#one");
+const carre2 = document.querySelector("#two");
+const carre3 = document.querySelector("#three");
+const carre4 = document.querySelector("#four");
 
-carre.addEventListener("click", function(){
+//creating array containing all squares
+const carres = [carre1, carre2, carre3, carre4];
+//for each element in the array, toggle class "squareClicked"
+carres.forEach((carre) => carre.addEventListener("click", function(){
     carre.classList.toggle("squareClicked");
-});
+})
+);
