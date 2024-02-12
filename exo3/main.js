47b12d3dbfad91e1c55f7376748cb806abe98f8e
AHdev67 <antoinehelfer.contact@gmail.com>
2024-02-12 16:30:48 +0100

    added commentary

--- exo3/main.js
+++ exo3/main.js
@@ -5,24 +5,28 @@ const cells = document.querySelectorAll(".cell");
 const board = document.querySelector("#container");
 const currentTurn = document.querySelector("#gameState");
 const longTableau = cells.length
+//compteur pour verifier le dernier tour et afficher le message de fin
 var count = 1
 
-// remplissage des cases
-
-
+    // remplissage des cases
     cells.forEach((cell) => cell.addEventListener("click", function(){
         console.log(count)
+        //TOUR JOUEUR 1
         if (tourJoueur){
+            //toggle de la class correspondant au bon joueur
             cell.classList.toggle("joueur1")
             cell.innerText = "X";
+            //affichage du message de fin si dernier tour
             if( count >= cells.length){
                 currentTurn.value = "Partie Terminée";
             }
+            //passer au prochain tour
             else{
                 changerTour();
                 afficherTour();
             }
         }
+        //TOUR JOUEUR 
         else{
             cell.classList.toggle("joueur2")
             cell.innerText = "O";
