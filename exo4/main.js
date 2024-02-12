c29fdffef638d12fa2b979a85a8747a495e47374
AHdev67 <antoinehelfer.contact@gmail.com>
2024-02-12 14:43:39 +0100

    Fixed add box function, added remove box feature, added turn box black on click feature

--- exo4/main.js
+++ exo4/main.js
@@ -1,5 +1,6 @@
 const board = document.querySelector("#container");
 
+//fonction de randomization de couleur tirée de stack overflow
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
@@ -7,14 +8,29 @@ function getRandomColor() {
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
+}
 
 const box = document.createElement("div");
 box.classList.add("square");
 
-board.addEventListener("keyup", (event) => {
-    if(event.isComposing || event.keyCode === 40){
-        box.style.backgroundColor = getRandomColor();
-        board.appendChild(box);
+window.addEventListener("keydown", checkKeyPress, false);
+
+function checkKeyPress(e) {
+    //initialisation de la nouvelle case
+    const newBox = box.cloneNode(true);
+    newBox.style.backgroundColor = getRandomColor();
+    
+    //SI flèche du bas, alors on ajoute une case
+    if (e.keyCode == '40'){
+        board.appendChild(newBox);
     }
-})
+    //SINON SI flèche du haut, retirer dernier élément de board
+    else if (e.keyCode == '38'){
+        board.lastElementChild.remove();
+    }
+
+    //SI on click sur une case, on la colorie en noir
+    newBox.addEventListener("click", function(){
+        newBox.style.backgroundColor = "rgb(0, 0, 0)";
+    })
+}
